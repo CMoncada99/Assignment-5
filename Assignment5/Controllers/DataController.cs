50eace8ebf9d7b49fa02622e2a7edcd29c8fd27a
Claudio Moncada <cmoncada@usf.edu>
2022-04-30 02:44:49 -0400

    Add project files.

---
+++ Assignment5/Controllers/DataController.cs
@@ -0,0 +1,20 @@
+﻿using Assignment5.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Assignment5.Controllers
+{
+    public class DataController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+       // public IActionResult Department()
+        //{
+            
+        //}
+    }
+    
+}
---
+++ Assignment5/Models/DataSF.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment5.Models
+
+{
+    public class Department
+    {
+        public int deptid { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class supplier
+    {
+        public int supplierId { get; set; }
+        public string suppliername { get; set; }
+        public string type { get; set; }
+        public string profit { get; set; }
+
+    }
+    public class contract
+    {
+        public int contractid { get; set; }
+        public decimal amount { get; set; }
+        public decimal payment { get; set; }
+        public decimal balance { get; set; }
+        public Department department { get; set; }
+        public supplier supplier { get; set; }
+
+    }
+}
